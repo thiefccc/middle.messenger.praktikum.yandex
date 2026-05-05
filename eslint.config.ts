7be8013a2220542a408aae71ed1fdc97797bb6ca
Roman Vasev <r.vasev@xsolla.com>
2026-05-05 16:48:57 +0500

    sprint_4: Infra + test

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,13 +5,41 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
+    ignores: ["dist/**", "coverage/**", "node_modules/**"],
+  },
+  {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     languageOptions: { globals: globals.browser },
     rules: {
       "no-unused-vars": "error",
-      "no-undef": "error"
-    }
+      "no-undef": "error",
+      curly: ["error", "all"],
+    },
   },
   tseslint.configs.recommended,
   js.configs.recommended,
+  {
+    files: ["**/*.{ts,mts,cts,tsx}"],
+    rules: {
+      "no-unused-vars": "off",
+      "no-undef": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  {
+    files: ["src/**/*.test.ts"],
+    languageOptions: { globals: { ...globals.browser, ...globals.jest } },
+  },
+  {
+    files: [
+      "vite.config.ts",
+      "jest.config.cjs",
+      "*.cjs",
+      "*.config.{js,ts,cjs,mjs}",
+    ],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
 ]);
